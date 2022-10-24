dc9905bebc0495a999d40934f7090d9ba7a0a23f
priyankabkharvani <114006766+priyankabkharvani@users.noreply.github.com>
2022-10-24 17:05:21 +0200

    init

---
+++ traceit!/traceit!/traceit_App.swift
@@ -0,0 +1,17 @@
+//
+//  traceit_App.swift
+//  traceit!
+//
+//  Created by Priyanka Bkharvani on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct traceit_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
